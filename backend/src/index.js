dc44651c12743bb737e21b2af9ee31f24ae3e2d4
Mir Hadi ali <mirhadiali62@gmail.com>
2026-05-02 10:21:13 +0500

    update made

--- backend/src/index.js
+++ backend/src/index.js
@@ -29,12 +29,12 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  const frontendPath = path.join(process.cwd(), "../frontend/dist");
+
+  app.use(express.static(frontendPath));
 
   app.use((req, res) => {
-    res.sendFile(
-      path.join(__dirname, "../frontend","dist","index.html")
-    );
+    res.sendFile(path.join(frontendPath, "index.html"));
   });
 }
 
