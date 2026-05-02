0a7aa123287235f40ba01b2d13551fd89cf2ad87
Mir Hadi ali <mirhadiali62@gmail.com>
2026-05-02 08:58:26 +0500

    project completed

---
+++ backend/src/lib/db.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const connectDB= async ()=>{
+    try {
+       const conn= await mongoose.connect(process.env.MONGODB_URI,)
+        console.log(`Connected to MongoDB ${conn.connection.host}`);
+        
+    } catch (error) {
+        console.log("Error connecting MongoDB",error);
+        
+    }
+}
+
+export default connectDB;
